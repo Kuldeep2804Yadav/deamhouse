12494e31f1f42fa868b1698aa11c9abf38082739
Kuldeep Yadav <kuldeep2804yadav@gmail.com>
2025-03-23 14:27:02 +0530

    fixed

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -113,19 +113,7 @@ export default function Profile() {
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-        <input
-          onChange={(e) => setFile(e.target.files[0])}
-          type='file'
-          ref={fileRef}
-          hidden
-          accept='image/*'
-        />
-        <img
-          onClick={() => fileRef.current.click()}
-          src={formData.avatar || currentUser.avatar}
-          alt='profile'
-          className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'
-        />
+       
         <input
           type='text'
           placeholder='Username'
